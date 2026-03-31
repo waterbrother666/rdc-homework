bc119912a8becf74f219c4594bbe243aba177e82
shui-ge-123666 <3418737753@qq.com>
2026-03-31 22:40:47 +0800

    feat:3.31update

--- index.js
+++ index.js
@@ -82,3 +82,66 @@ flipImages.forEach(img => {
 });
 
 //--------------------------------------------------
+
+//--------------------视频轮播以及活跃点实现--------------------
+document.addEventListener('DOMContentLoaded', function(){
+    const carouseItems = document.querySelectorAll('.carousel-item');
+    const dots = document.querySelectorAll('.dot');
+    let currentIndex = 0;//指引轮播的视频的关键
+    const totalItems = carouseItems.length;
+    let autoPlayInterval;
+    //设置一个关闭
+
+
+    //初始化
+    function updateCarousel(){
+        carouseItems.forEach(item => {
+            item.classList.remove('active', 'next', 'prev','hide');
+            const video = item.querySelector('video');
+            video.pause();
+        });
+        dots.forEach(dot => dot.classList.remove('active'));
+
+        let nextIndex = (currentIndex + 1) % totalItems;//加1表示向右
+        let prevIndex = (currentIndex - 1 + totalItems) % totalItems;//减1表示向左
+        //存在疑问+1和-1的问题
+        carouseItems[currentIndex].classList.add('active');
+        carouseItems[nextIndex].classList.add('next');
+        carouseItems[prevIndex].classList.add('prev');
+
+        carouseItems[currentIndex].querySelector('video').play();
+
+        carouseItems.forEach((item, index) => {
+            if(index !== currentIndex && index !== nextIndex && index !== prevIndex){
+                item.classList.add('hide');
+            }
+        });
+        if(dots[currentIndex]) dots[currentIndex].classList.add('active');
+    }
+    
+    function startAutoPlay(){
+        //如果已经有定时器了 删掉防止出现超速bug
+        if (autoPlayInterval) clearInterval(autoPlayInterval);
+        autoPlayInterval = setInterval(function(){
+            currentIndex = (currentIndex + 1) % totalItems;
+            updateCarousel();
+        }, 4000); 
+    }
+
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', function(){
+            currentIndex = index;
+            updateCarousel();
+            startAutoPlay();
+        });
+    });
+
+    updateCarousel();
+    startAutoPlay();
+});
+
+
+
+
+//------------------------------------------------------------
